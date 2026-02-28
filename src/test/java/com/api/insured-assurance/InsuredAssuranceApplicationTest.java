ea6eab57839edc6d0984b6f7c0df261460841255
learn-grow-repeat <swathymenon.316@gmail.com>
2026-02-28 17:43:29 +0000

    Create InsuredAssuranceApplicationTest.java

---
+++ src/test/java/com/api/insured-assurance/InsuredAssuranceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.api.insured-assurance
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuredAssuranceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
